f778aef18ee71852b67a4014fdf62c624ee556f5
Vasu Gadde <vasu.gadde@JHS.LOCAL>
2025-12-13 12:25:22 +0530

    Uploading application again

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ app.add_middleware(
 
 client = MongoClient(os.getenv("MONGO_CONNECTION_STRING"))
 print(os.getenv("MONGO_CONNECTION_STRING"))
-
 db = client["HR_Helpdesk"]
 ticketscol = db["Tickets"]
 admins = db["Admins"]
